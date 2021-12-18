3cf4ad68e8fd255d09e9816dd88c8f42e650c846
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-12-19 00:18:31 +0200

    update repo url

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='ConnectAPI',
-    author_email='ConnectAPI@gmail.com',
+    author_email='connectapi.org@gmail.com',
     maintainer='ConnectAPI',
-    maintainer_email='ConnectAPI@gmail.com',
-    url='https://github.com/APIConnect',
+    maintainer_email='connectapi.org@gmail.com',
+    url='https://github.com/ConnectAPI/python-core-client',
     keywords=['connectapi', 'ConnectAPI', 'web', 'client', 'core'],
     install_requires=['requests'],
     classifiers=[
