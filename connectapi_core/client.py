37e33b19276cb986b9c7d7013351e5613750c1ae
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-12-18 20:23:32 +0200

    full update

--- connectapi_core/client.py
+++ connectapi_core/client.py
@@ -2,7 +2,10 @@ from urllib.parse import urljoin
 from requests import Session, Response
 
 from .singleton import SingletonMeta
-from .exceptions import BadTokenException, ClientException
+from .exceptions import BadTokenException
+
+
+AUTH_HEADER = "x-access-token"
 
 
 class Client(metaclass=SingletonMeta):
@@ -12,7 +15,7 @@ class Client(metaclass=SingletonMeta):
         self.__refresh_token = refresh_token
         self.__session = Session()
 
-    def __get_refreshed_token(self):
+    def _refresh_token(self):
         url = urljoin(self.__url, 'internal/auth/refresh')
         response = self.__session.post(url, params={"refresh_token": self.__refresh_token})
         if response.status_code == 401:
@@ -20,11 +23,19 @@ class Client(metaclass=SingletonMeta):
         self.__token = response.text
 
     def request(self, method: str, service_path_prefix: str, path, **kwargs) -> Response:
+        headers = kwargs.get("headers", {})
+        headers.update({AUTH_HEADER: self.__token})
+        kwargs["headers"] = headers
         response = self.__session.request(method, service_path_prefix, path, **kwargs)
-        if response.status_code == 401 and response.headers.get("WWW-Authenticate", None) == "Bearer":
-            self.__get_refreshed_token()
+        if response.headers.get("x-auth-exception", None) == "Expired":
+            self._refresh_token()
+            kwargs["headers"][AUTH_HEADER] = self.__token
             response = self.__session.request(method, service_path_prefix, path, **kwargs)
-
-        if response.status_code >= 300:
-            raise ClientException(f'{response.status_code} {response.text}')
+        elif response.headers.get("x-auth-exception", None) == "Invalid":
+            raise BadTokenException("invalid token")
+        elif response.headers.get("x-auth-exception", None) == "Not Authorized":
+            required_permission = response.headers.get("x-required-scope", None)
+            raise BadTokenException(
+                f"you do not have to permissions to make that request (required permission: {required_permission})"
+            )
         return response
